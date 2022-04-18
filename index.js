abf9b4e31299908bb2808638990cdb33234baf6e
Teewai57 <bobtoyoabasi@gmail.com>
2022-04-18 20:30:26 +0100

    ..

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-new Vue ({
-    el: '#taskList',
-    data: {
-        title: 'to do list',
-        tasks: [
-            {name:'Today : Internal Meeting' },
-            {name: 'Tomorrow : Read a book'},
-            {name: 'Daily : Event Joins'},
-        ]
+
+const todoApp = Vue.createApp({
+    data() {
+        return {
+            title: "Todo List",
+            tasks: [
+                { name: 'Today : Internal Meeting' },
+                { name: 'Tomorrow : Read a book' },
+                { name: 'Daily : Event Joins' },
+            ]
+
+        }
     },
     methods: {
-        newItem: function() {
-            if(!this.tasks.name) {
+        newItem: function () {
+            if (!this.tasks.name) {
                 return
             }
-            this.tasks.push ({
+            this.tasks.push({
                 name: this.tasks.name,
                 del: ''
             });
@@ -23,4 +26,6 @@ new Vue ({
             this.tasks.splice(this.tasks.indexOf(task), 1)
         }
     }
+
 })
+todoApp.mount("#todoApp");
